9183134be577565d930150cbd5ffe30e14112151
Flynn <wphestiraid@gmail.com>
2018-02-12 16:31:49 +0900

    초기 state 정의 및 Form 기능 구현
    - 텍스트 내용이 변경되면 state 업데이트
    - 버튼이 클릭되면 새로운 todo를 생성 후 todos 업데이트
    - 인풋에서 Enter 누르면 버튼을 클릭한 것과 동일한 작업 진행

--- src/App.js
+++ src/App.js
@@ -4,10 +4,59 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+    id = 3;
+
+    state = {
+        input: '',
+        todos: [
+            { id: 0, text: ' 리액트 소개', checked: false },
+            { id: 1, text: ' 리액트 소개', checked: true },
+            { id: 2, text: ' 리액트 소개', checked: false }
+        ]
+    }
+
+    _handleChange = (e) => {
+        this.setState({
+            input: e.target.value
+        });
+    }
+
+    _handleCreate = () => {
+        const { input, todos } = this.state;
+        this.setState({
+            input: '',
+            todos: todos.concat({
+                id: this.id++,
+                text: input,
+                checked: false
+            })
+        });
+    }
+
+    _handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            this._handleCreate();
+        }
+    }
+
     render() {
+        const { input } = this.state;
+        const {
+            _handleChange,
+            _handleCreate,
+            _handleKeyPress
+        } = this;
+
         return (
             <div>
-                <TodoListTemplate form={<Form />}>
+                <TodoListTemplate form={(
+                    <Form
+                        value={input}
+                        onKeyPress={_handleKeyPress}
+                        onChange={_handleChange}
+                        onCreate={_handleCreate}
+                    />
+                )}>
                     <TodoItemList />
                 </TodoListTemplate>
             </div>
