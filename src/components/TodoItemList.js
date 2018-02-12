40f5fc9bf9be37b7803cfc0b3f12cd3077c3a000
Flynn <wphestiraid@gmail.com>
2018-02-12 16:16:25 +0900

    TodoItem 컴포넌트 작성
    - checked가 true일 경우 우측에 체크마크를 보여줌
    - 마우스가 올려져있을 경우 좌측에 엑스마크를 보여줌
    - 엑스마크가 클릭되면 삭제됨
    - e.stopPropagation으로 이벤트의 확산을 멈춤

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoItemList extends Component {
     render() {
@@ -6,7 +7,9 @@ class TodoItemList extends Component {
 
         return (
             <div>
-
+                <TodoItem text="TEST" />
+                <TodoItem text="TEST" />
+                <TodoItem text="TEST" />
             </div>
         );
     }
