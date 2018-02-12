34ec344f84247d7b688bd47bd2ed91e27ff3f300
Flynn <wphestiraid@gmail.com>
2018-02-12 15:58:47 +0900

    TodoItemList 컴포넌트 작성
    - TodoItem 컴포넌트를 여러 개 렌더링해주는 역할을 함
    - 클래스형 컴포넌트로 작성

---
+++ src/components/TodoItemList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TodoItemList extends Component {
+    render() {
+        const { todos, onToggle, onRemove } = this.props;
+
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;
